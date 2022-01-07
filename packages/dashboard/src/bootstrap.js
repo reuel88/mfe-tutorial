d127c0b159f05443fd190699e661cdf9a546935e
Reuel Teodoro <12149910+reuel88@users.noreply.github.com>
2022-01-07 15:03:22 +1100

    update the workflow again

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -11,8 +11,9 @@ const mount = (el) => {
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_dashboard-dev-root');
 
-    if (devRoot)
+    if (devRoot) {
         mount(devRoot)
+    }
 }
 
 // We are running through container and we should export the mount function
