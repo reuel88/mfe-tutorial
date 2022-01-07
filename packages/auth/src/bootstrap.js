b55f5046fed95c9a024119f0dda8368a4fff6280
Reuel Teodoro <12149910+reuel88@users.noreply.github.com>
2022-01-07 12:09:53 +1100

    quick auth function

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,14 +4,14 @@ import {createMemoryHistory, createBrowserHistory} from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el, {onNavigate, defaultHistory, initialPath}) => {
+const mount = (el, {onNavigate, onSignIn, defaultHistory, initialPath}) => {
     const history = defaultHistory || createMemoryHistory({
         initialEntries:[initialPath]
     });
     if (onNavigate) history.listen(onNavigate);
 
     ReactDom.render(
-        <App history={history} />,
+        <App onSignIn={onSignIn} history={history} />,
         el
     )
 
